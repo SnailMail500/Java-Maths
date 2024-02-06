c485bde2905e66e2b4d3ff0d2fd68398541d4586
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-02-06 16:49:02 +0000

    Added some semicolons that I forgot and then finished the maths chapter

--- Main.java
+++ Main.java
@@ -2,8 +2,13 @@ public class Main {
     public static void main (String[] args){
         //"Math" is the maths method in Java
 
-        System.out.println(Math.max(5, 10))//returns 10 as its the higher of the two numbers
-        System.out.println(Math.min(5, 10))//returns 5 as is the lower of the two numbers
-        
+        System.out.println(Math.max(5, 10));//returns 10 as its the higher of the two numbers
+        System.out.println(Math.min(5, 10));//returns 5 as is the lower of the two numbers
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.random());//this makes a pseudorandom number
+        int rand = (int) (Math.random() * 9);//this makes a pseudorandom number between zero and eight
+        //don't forget that most languages are upper bound exclusive
+        System.out.println(rand);
+        System.out.println(rand);//ooh, random!
     }
 }
