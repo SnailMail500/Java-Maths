7a6836a92163f36a076e5f77b8edee001272222c
Alex <83418697+SnailMail500@users.noreply.github.com>
2024-02-06 16:39:03 +0000

    First commit so that this repo can go onto GitHub

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args){
+        //"Math" is the maths method in Java
+
+        System.out.println(Math.max(5, 10))//returns 10 as its the higher of the two numbers
+        System.out.println(Math.min(5, 10))//returns 5 as is the lower of the two numbers
+        
+    }
+}
